f15a98787de6db324b82b4914c70947fc4c79f33
kberg721 <kberg721@hotmail.com>
2016-05-14 20:49:38 -0700

    Add files via upload

--- nodePractice/demoForms.js
+++ nodePractice/demoForms.js
@@ -6,6 +6,7 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', 3000);
