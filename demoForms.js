30d93840333e9d652395dc9466098bae88c510ca
kberg721 <kberg721@hotmail.com>
2016-05-14 20:20:00 -0700

    Add files via upload

---
+++ demoForms.js
@@ -0,0 +1,53 @@
+var express = require('express');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/',function(req,res){
+  res.render('home.handlebars') //we can omit the .handlebars extension as we do below
+});
+
+app.get('/other-page',function(req,res){
+  res.render('other-page');
+});
+
+app.get('/get-loopback', function(req, res) {
+  var qParams = [];
+  for (var p in req.query) {
+    qPArams.push({'name':p, 'value':req.query[p]})
+  }
+  var context = {};
+  context.dataList = qParams;
+  res.render('get-loopback', context);
+}); 
+
+function genNumber(){
+  var toDisplay = {};
+  toDisplay.number = Math.random();
+  return toDisplay;
+}
+
+app.get('/random-number',function(req,res){
+  var randomNum = Math.random();
+  res.render('random-number', genNumber());
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.type('plain/text');
+  res.status(500);
+  res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
