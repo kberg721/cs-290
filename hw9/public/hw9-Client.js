111ee29b037197d873aa9e73b9de0b5b9cfb188f
kberg721 <kberg721@hotmail.com>
2016-05-25 21:35:42 -0700

    Add files via upload

--- hw9/public/hw9-Client.js
+++ hw9/public/hw9-Client.js
@@ -4,20 +4,18 @@ function bindButtons() {
 	document.getElementById('newWorkOutButton').addEventListener('click', function(event){
     var req = new XMLHttpRequest();
 
+    console.log("hello");
     req.open('POST', 'http://localhost:3000', true);
     req.setRequestHeader('Content-Type', 'application/json');
     req.addEventListener('load',function(){
 	    if(req.status >= 200 && req.status < 400){
-	      var response2 = JSON.parse(req.responseText);
-	      console.log(response2);
+	      var response = JSON.parse(req.responseText);
+	      console.log(response);
 	    } else {
-	      console.log("Error in network request: " + request2.statusText);
+	      console.log("Error in network request: " + request.statusText);
 	    }
 	});
-    req2.send(null);
+    req.send(null);
     event.preventDefault();
-
-
-
 	});
 }
