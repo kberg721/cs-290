633ab4c333163e3602b6543a441472cabd743e3f
kberg721 <kberg721@hotmail.com>
2016-05-26 13:37:02 -0700

    Add files via upload

--- hw9/public/hw9-Client.js
+++ hw9/public/hw9-Client.js
@@ -2,21 +2,21 @@ document.addEventListener('DOMContentLoaded', bindButtons);
 
 function bindButtons() {
 	document.getElementById('newWorkout').addEventListener('click', function(event){
-    var req = new XMLHttpRequest();
-    var payload = {};
-    payload.name = document.getElementById('workoutName').value;
-    req.open('POST', 'http://52.37.58.94:3000/', true);
-    req.setRequestHeader('Content-Type', 'application/json');
-    req.addEventListener('load',function(){
-	    if(req.status >= 200 && req.status < 400){
-	      var response = JSON.parse(req.responseText);
-	      console.log(response);
-	    } else {
-	      console.log("Error in network request: " + request.statusText);
-	    }
-	});
-	console.log(JSON.stringify(payload));
-	req.send(JSON.stringify(payload));
-    event.preventDefault();
+	    var req = new XMLHttpRequest();
+	    var payload = {};
+	    payload.name = document.getElementById('workoutName').value;
+	    req.open('POST', 'http://httpbin.org/post', true);
+	    req.setRequestHeader('Content-Type', 'application/json');
+	    req.addEventListener('load',function(){
+		    if(req.status >= 200 && req.status < 400){
+		      var response = JSON.parse(req.responseText);
+		      console.log(response);
+		    } else {
+		      console.log("Error in network request: " + request.statusText);
+		    }
+		});
+		console.log(JSON.stringify(payload));
+		req.send(JSON.stringify(payload));
+	    event.preventDefault();
 	});
 }
