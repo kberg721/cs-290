712bbd4f98fceb19e81884b9a5ff12405640a2e9
kberg721 <kberg721@hotmail.com>
2016-05-25 18:10:04 -0700

    Add files via upload

---
+++ hw9/hw9.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var mysql = require('./dbcon.js');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 3000);
+
+app.get('/',function(req,res,next){
+  var context = {};
+  mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = JSON.stringify(rows);
+    res.render('home', context);
+  });
+});
+
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  //replace your connection pool with the your variable containing the connection pool
+   mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){ 
+    var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+    mysql.pool.query(createString, function(err){
+      context.results = "Table reset";
+      res.render('home',context);
+    })
+  });
+});
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
