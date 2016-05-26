90aa087ea15409b5cd20a4c24193dde8db1e809e
kberg721 <kberg721@hotmail.com>
2016-05-26 14:56:47 -0700

    Add files via upload

--- hw9/public/hw9-Client.js
+++ hw9/public/hw9-Client.js
@@ -18,7 +18,7 @@ function bindButtons() {
 	    req.addEventListener('load',function(){
 		    if(req.status >= 200 && req.status < 400){
 		      var response = JSON.parse(req.responseText);
-		      console.log(response);
+		      console.log("our response: " + response);
 		    } else {
 		      console.log("Error in network request: " + req.statusText);
 		    }
