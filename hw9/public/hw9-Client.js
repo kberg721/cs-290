57276042be1e6510bd4ed372c8b47dbe3ffe3ffc
kberg721 <kberg721@hotmail.com>
2016-05-26 19:04:32 -0700

    Add files via upload

--- hw9/public/hw9-Client.js
+++ hw9/public/hw9-Client.js
@@ -30,28 +30,13 @@ function setUpPage() {
 	});
 
 	var deleteBtns = document.getElementsByClassName("deleteBtn");
-	console.log(deleteBtns);
 	for(var i = 0; i < deleteBtns.length; i++) {
-		deleteBtns[i].addEventListener('click', function(event){
-			return function(event) {
-				var req = new XMLHttpRequest();
-			    var payload = {};
-			    payload.btn = "Delete";
-			    parload.id = i + 1;
-			    req.open('POST', 'http://52.37.58.94:3000/', true);
-			    req.setRequestHeader('Content-Type', 'application/json');
-			    req.addEventListener('load',function(){
-				    if(req.status >= 200 && req.status < 400){
-				      var response = JSON.parse(req.responseText);
-				      populateTable(response);
-				    } else {
-				      console.log("Error in network request: " + req.statusText);
-				    }
-				});
-				req.send(JSON.stringify(payload));
-			    event.preventDefault();
+		var listItem = deleteBtns[i];
+		listItem.onclick = (function(value) {
+			return function() {
+				alert(value + 1);
 			}
-		});
+		})(i);
 	}
 	
 }
