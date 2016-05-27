8c827b9533e5ccbce62b294574b3bcd675bdd030
kberg721 <kberg721@hotmail.com>
2016-05-26 21:47:22 -0700

    Add files via upload

--- hw9/hw9.js
+++ hw9/hw9.js
@@ -49,7 +49,7 @@ app.post('/', function(req, res, next) {
         next(err);
         return;
       }
-      res.status(200).send(JSON.stringify(rows));
+      res.status(200).send(rows);
     });
   }
 
@@ -61,7 +61,7 @@ app.post('/', function(req, res, next) {
         next(err); 
         return;
       }
-       res.status(200).send(JSON.stringify(rows));
+       res.status(200).send(rows);
     });
   }
   
