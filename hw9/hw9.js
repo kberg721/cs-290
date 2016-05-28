169dfdb75a37ee43a6fc43af184ccbddb27c4acf
kberg721 <kberg721@hotmail.com>
2016-05-27 21:20:12 -0700

    Add files via upload

--- hw9/hw9.js
+++ hw9/hw9.js
@@ -53,7 +53,7 @@ app.post('/', function(req, res, next) {
   }
 
   if(req.body.btn == "Delete") {
-    console.log(req.body.id);
+    console.log(req.param("id"));
     mysql.pool.query("DELETE FROM workouts WHERE id = ?", 
       [req.body.id], function(err, rows, fields) {
       if(err) {
