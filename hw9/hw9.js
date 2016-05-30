802394c8e541a73db02c8a9bfc5e8610a20b7ab7
kberg721 <kberg721@hotmail.com>
2016-05-30 15:02:45 -0700

    Add files via upload

--- hw9/hw9.js
+++ hw9/hw9.js
@@ -25,7 +25,7 @@ app.get('/',function(req,res,next){
       context.results = rows;
       console.log("this assignment");
       console.log(rows);
-      res.redirect('http://google.com', context);
+      res.redirect('http://google.com');
     });
   } else {
     mysql.pool.query('SELECT * FROM workouts', function(err, rows, fields){
