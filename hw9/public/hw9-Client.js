237757378fee7e50cc0660647f0d90498ace5f84
kberg721 <kberg721@hotmail.com>
2016-05-30 16:26:39 -0700

    Add files via upload

--- hw9/public/hw9-Client.js
+++ hw9/public/hw9-Client.js
@@ -4,33 +4,67 @@ function setUpPage() {
 	
 	attachFormFunctions();
 
-	document.getElementById('newWorkoutBtn').addEventListener('click', function(event){
-	    var req = new XMLHttpRequest();
-	    var payload = {};
-	    payload.btn = "Add";
-	    payload.name = document.getElementById('workoutName').value;
-	    payload.reps = document.getElementById('workoutReps').value;
-	    payload.weight = document.getElementById('workoutWeight').value;
-	    payload.date = document.getElementById('workoutDate').value;
-	    if(document.getElementById('lbsCheck').checked) {
-	    	payload.unit = "lbs";
-	    } else {
-	    	payload.unit = "kgs";
-	    }
-	    req.open('POST', 'http://52.37.58.94:3000/', true);
-	    req.setRequestHeader('Content-Type', 'application/json');
-	    req.addEventListener('load',function(){
-		    if(req.status >= 200 && req.status < 400){
-		      var response = JSON.parse(req.responseText);
-		      populateTable(response);
-		      attachFormFunctions();
+	if(document.getElementById('newWorkoutBtn') != null) {
+		document.getElementById('newWorkoutBtn').addEventListener('click', function(event){
+		    var req = new XMLHttpRequest();
+		    var payload = {};
+		    payload.btn = "Add";
+		    payload.name = document.getElementById('workoutName').value;
+		    payload.reps = document.getElementById('workoutReps').value;
+		    payload.weight = document.getElementById('workoutWeight').value;
+		    payload.date = document.getElementById('workoutDate').value;
+		    if(document.getElementById('lbsCheck').checked) {
+		    	payload.unit = "lbs";
 		    } else {
-		      console.log("Error in network request: " + req.statusText);
+		    	payload.unit = "kgs";
 		    }
+		    req.open('POST', 'http://52.37.58.94:3000/', true);
+		    req.setRequestHeader('Content-Type', 'application/json');
+		    req.addEventListener('load',function(){
+			    if(req.status >= 200 && req.status < 400){
+			      var response = JSON.parse(req.responseText);
+			      populateTable(response);
+			      attachFormFunctions();
+			    } else {
+			      console.log("Error in network request: " + req.statusText);
+			    }
+			});
+			req.send(JSON.stringify(payload));
+		    event.preventDefault();
 		});
-		req.send(JSON.stringify(payload));
-	    event.preventDefault();
-	});
+	}
+
+	if(document.getElementById('editWorkoutBtn') != null) {
+		document.getElementById('editWorkoutBtn').addEventListener('click', function(event){
+		    var req = new XMLHttpRequest();
+		    var payload = {};
+		    payload.btn = "Edit";
+		    payload.name = document.getElementById('workoutName').value;
+		    payload.reps = document.getElementById('workoutReps').value;
+		    payload.weight = document.getElementById('workoutWeight').value;
+		    payload.date = document.getElementById('workoutDate').value;
+		    if(document.getElementById('lbsCheck').checked) {
+		    	payload.unit = "lbs";
+		    } else {
+		    	payload.unit = "kgs";
+		    }
+		    req.open('POST', 'http://52.37.58.94:3000/', true);
+		    req.setRequestHeader('Content-Type', 'application/json');
+		    /*
+		    req.addEventListener('load',function(){
+			    if(req.status >= 200 && req.status < 400){
+			      var response = JSON.parse(req.responseText);
+			      populateTable(response);
+			      attachFormFunctions();
+			    } else {
+			      console.log("Error in network request: " + req.statusText);
+			    }
+			});
+			*/
+			req.send(JSON.stringify(payload));
+		    event.preventDefault();
+		});
+	}
 	
 }
 
