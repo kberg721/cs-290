35769561c6b26bf7092f20487703b25062a86de0
kberg721 <kberg721@hotmail.com>
2016-05-30 12:53:57 -0700

    Add files via upload

--- hw9/public/hw9-Client.js
+++ hw9/public/hw9-Client.js
@@ -69,16 +69,17 @@ function attachFormFunctions() {
 			    payload.id = item.parentElement.id.value;
 			    req.open('POST', 'http://52.37.58.94:3000/edit-data', true);
 			    req.setRequestHeader('Content-Type', 'application/json');
+			    /*
 			    req.addEventListener('load',function(){
 				    if(req.status >= 200 && req.status < 400){
 				      //var response = JSON.parse(req.responseText);
-				      console.log("hi");
 				      //populateTable(response);
 				      //attachFormFunctions();
 				    } else {
 				      console.log("Error in network request: " + req.statusText);
 				    }
 				});
+				*/
 				req.send(JSON.stringify(payload));
 			    event.preventDefault();
 			};
